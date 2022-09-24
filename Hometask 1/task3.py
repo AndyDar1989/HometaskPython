7bd8ad98b1eed550983fa668566301cc7dbe4678
AndrewDar <geologalone@gmail.com>
2022-09-24 09:58:55 +0300

    all tasks are checked and formatted

--- Hometask 1/task3.py
+++ Hometask 1/task3.py
@@ -1,23 +1,23 @@
-#3. Напишите программу, которая принимает на вход координаты точки (X и Y),
+# 3. Напишите программу, которая принимает на вход координаты точки (X и Y),
 # причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка (или на какой оси она находится).
-#*Пример:*
-#- x=34; y=-30 -> 4
-#- x=2; y=4-> 1
-#- x=-34; y=-30 -> 3
+# *Пример:*
+# - x=34; y=-30 -> 4
+# - x=2; y=4-> 1
+# - x=-34; y=-30 -> 3
 
 x = int(input('enter X-coordinate: '))
 y = int(input('enter Y-coordinate: '))
-if x>0 and y>0:
+if x > 0 and y > 0:
     print('first quarter')
-elif x>0 and y<0:
+elif x > 0 and y < 0:
     print('second quarter')
-elif x<0 and y<0:
+elif x < 0 and y < 0:
     print('third quarter')
-elif x<0 and y>0:
+elif x < 0 and y > 0:
     print('fourth quarter')
-elif x==0 and y !=0:
-    print('Y-axe')
-elif y==0 and x !=0:
-    print('X-axe')
-elif x==0 and y==0 :
-    print('zero point')            
+elif x == 0 and y != 0:
+    print('Y-axis')
+elif y == 0 and x != 0:
+    print('X-axis')
+elif x == 0 and y == 0:
+    print('zero point')
--- Hometask 1/task4.py
+++ Hometask 1/task4.py
@@ -1,15 +1,14 @@
-#4. Напишите программу, которая по заданному номеру четверти,
+# 4. Напишите программу, которая по заданному номеру четверти,
 # показывает диапазон возможных координат точек в этой четверти (x и y).
 
-n = int(input('enter quarters number: '))
-if n==1:
+n = int(input('enter quarter number: '))
+if n == 1:
     print('x>0, y>0')
-elif n==2:
+elif n == 2:
     print('x>0 and y<0')
-elif n==3:
+elif n == 3:
     print('x<0 and y<0')
-elif n==4:
+elif n == 4:
     print('x<0 and y>0')
 else:
-    print('incorrect value')    
-    
+    print('incorrect value')
