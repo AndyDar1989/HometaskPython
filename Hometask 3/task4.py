3eb8fdd7cb43a55ad3ea35a843a772fa60a7f7a1
AndrewDar <geologalone@gmail.com>
2022-10-05 15:50:38 +0300

    task4 complited

---
+++ Hometask 3/task4.py
@@ -0,0 +1,38 @@
+# 4.* Задайте список из произвольных вещественных чисел, количество задаёт пользователь.
+# Напишите программу, которая найдёт разницу между максимальным
+# и минимальным значением дробной части элементов.
+# in  5
+# out [5.16, 8.62, 6.57, 7.92, 9.22]
+# Min: 0.16, Max: 0.92. Difference: 0.76
+
+import random
+
+
+def random_list(min, max, count):
+    new_list = []
+    for i in range(count):
+        value = round(random.uniform(min, max), 2)
+        new_list.append(value)
+    return new_list
+
+
+def diff_min_max(new_list: list):
+    for i in range(len(new_list)):
+        new_list[i] = round((new_list[i] % 1), 2)
+    min = new_list[0]
+    max = new_list[0]
+    for i in range(1, len(new_list)):
+        if new_list[i] < min:
+            min = new_list[i]
+        elif new_list[i] > max:
+            max = new_list[i]
+    diff = round((max-min), 2)
+    print(min)
+    print(max)
+    print(diff)
+
+
+c = int(input('enter count of numbers:'))
+my_list = random_list(0.0, 10.0, c)
+print(my_list)
+diff_min_max(my_list)
